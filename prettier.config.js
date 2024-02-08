1978007b6fef8278498186561616b2505e27f26f
Joris Mak <47725838+jorismak@users.noreply.github.com>
2024-02-08 12:06:47 +0100

    try the shopify-liquid prettier plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,19 +1,13 @@
 const tailwindcssPlugin = import("prettier-plugin-tailwindcss");
+import liquidPlugin from "@shopify/prettier-plugin-liquid";
 
 export default {
     plugins: [
+		liquidPlugin,
 	  tailwindcssPlugin
 	],
     tabWidth: 4,
     semi: true,
     singleQuote: false,
-    printWidth: 120,
-	overrides: [
-		{
-			files: '*.twig',
-			options: {
-				parser: 'html',
-			},
-		},
-	],
+    printWidth: 120
 };
