d2cf923ccdc5aa40bd62baabd08b94d95bde08f6
Joris Mak <joris.mak@orangevalley.nl>
2024-02-08 11:08:04 +0100

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+const twigMelody = import("prettier-plugin-twig-melody");
+const tailwind = import("prettier-plugin-tailwindcss");
+
+export default { 
+	plugins: [
+	  twigMelody,
+	  tailwind,
+	],
+    twigMelodyPlugins: ['node_modules/prettier-plugin-twig-enhancements'],
+    tabWidth: 4,
+	semi: true,
+	singleQuote: false,
+    printWidth: 120,
+    twigPrintWidth: 120,
+    twigFollowOfficialCodingStandards: true,
+    twigAlwaysBreakObjects: false
+};
