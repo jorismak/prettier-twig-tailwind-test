2e665353e010f1c86ea06c360b3fc9d7d2b4a7ef
Joris Mak <47725838+jorismak@users.noreply.github.com>
2024-02-08 11:46:35 +0100

    remove comments and cleanup config

--- prettier.config.js
+++ prettier.config.js
@@ -1,19 +1,13 @@
-import twigMelody from "prettier-plugin-twig-melody";
 const tailwindcssPlugin = import("prettier-plugin-tailwindcss");
 
 export default {
     plugins: [
-//	  twigMelody,
 	  tailwindcssPlugin
 	],
-    //twigMelodyPlugins: ["node_modules/prettier-plugin-twig-enhancements"],
     tabWidth: 4,
     semi: true,
     singleQuote: false,
     printWidth: 120,
-    //twigPrintWidth: 120,
-    //twigFollowOfficialCodingStandards: true,
-    //twigAlwaysBreakObjects: false,
 	overrides: [
 		{
 			files: '*.twig',
