1350aeee910e26cfe223b6b4866c26e7ad271405
Joris Mak <47725838+jorismak@users.noreply.github.com>
2024-02-08 11:43:53 +0100

    try to format twig files as plain html files

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,25 @@
-const twigMelody = import("prettier-plugin-twig-melody");
-const tailwind = import("prettier-plugin-tailwindcss");
+import twigMelody from "prettier-plugin-twig-melody";
+const tailwindcssPlugin = import("prettier-plugin-tailwindcss");
 
 export default {
-    plugins: [tailwind, twigMelody],
-    twigMelodyPlugins: ["node_modules/prettier-plugin-twig-enhancements"],
+    plugins: [
+//	  twigMelody,
+	  tailwindcssPlugin
+	],
+    //twigMelodyPlugins: ["node_modules/prettier-plugin-twig-enhancements"],
     tabWidth: 4,
     semi: true,
     singleQuote: false,
     printWidth: 120,
-    twigPrintWidth: 120,
-    twigFollowOfficialCodingStandards: true,
-    twigAlwaysBreakObjects: false,
+    //twigPrintWidth: 120,
+    //twigFollowOfficialCodingStandards: true,
+    //twigAlwaysBreakObjects: false,
+	overrides: [
+		{
+			files: '*.twig',
+			options: {
+				parser: 'html',
+			},
+		},
+	],
 };
