6090124d635473df8821ba56277c90ccb4e99dcd
Joris Mak <47725838+jorismak@users.noreply.github.com>
2024-02-08 11:20:30 +0100

    Get tailwind going locally

--- prettier.config.js
+++ prettier.config.js
@@ -1,17 +1,14 @@
 const twigMelody = import("prettier-plugin-twig-melody");
 const tailwind = import("prettier-plugin-tailwindcss");
 
-export default { 
-	plugins: [
-	  twigMelody,
-	  tailwind,
-	],
-    twigMelodyPlugins: ['node_modules/prettier-plugin-twig-enhancements'],
+export default {
+    plugins: [twigMelody, tailwind],
+    twigMelodyPlugins: ["node_modules/prettier-plugin-twig-enhancements"],
     tabWidth: 4,
-	semi: true,
-	singleQuote: false,
+    semi: true,
+    singleQuote: false,
     printWidth: 120,
     twigPrintWidth: 120,
     twigFollowOfficialCodingStandards: true,
-    twigAlwaysBreakObjects: false
+    twigAlwaysBreakObjects: false,
 };
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*.twig"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
